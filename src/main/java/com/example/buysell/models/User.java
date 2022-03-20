40defdecebbee7e1db747fe5de644174d20976a5
Dima <soloveid87@mail.ru>
2022-03-20 21:53:39 +1000

    admin panel

--- src/main/java/com/example/buysell/models/User.java
+++ src/main/java/com/example/buysell/models/User.java
@@ -40,7 +40,10 @@ public class User implements UserDetails {
     @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER, mappedBy = "user")
     private List<Product> products = new ArrayList<>();
     private LocalDateTime dateOfCreated;
-
+    
+    public boolean isAdmin(){
+        return roles.contains(Role.ROLE_ADMIN);
+    }
 
     @PrePersist
     private void init() {
